846ff11a5969cc0b59b23fcaa66c18d318784d6e
Knut Sælensminde <kwsdev@fastmail.fm>
2016-06-15 00:13:02 +0200

    Test

---
+++ menu setup/Assets/Scripts/MenuButtonController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonController : MonoBehaviour
+{
+
+    public Canvas Menu;
+    public Button NewButton;
+    public Button ExitButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        Menu = Menu.GetComponent<Canvas>();
+        NewButton = NewButton.GetComponent<Button>();
+        ExitButton = ExitButton.GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log("starting");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
